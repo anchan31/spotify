ff368715fdb62d39dba54444d27cfb9f1cc649a5
trikon-mswil <151496115+trikon-mswil@users.noreply.github.com>
2024-01-04 22:33:13 +0530

    Update music-list.js

--- music-list.js
+++ music-list.js
@@ -2,121 +2,121 @@ let allMusic = [
     {
       name: "Apna Bana Le",
       artist: "Arjit Singh",
-      img: "1",
-      src: "1"
+      img: "img-1",
+      src: "music-1"
     },
     {
       name: "Players",
       artist: "Badshah, Karan Aujla",
-      img: "2",
-      src: "2"
+      img: "img-2",
+      src: "music-2"
     },
     {
       name: "Closer X Kesariya",
       artist: "Sagar Swarap",
-      img: "3",
-      src: "3"
+      img: "img-3",
+      src: "music-3"
     },
     {
       name: "Passori",
       artist: "Coke Studio Pak",
-      img: "4",
-      src: "4"
+      img: "img-4",
+      src: "music-4"
     },
     {
       name: "Dilawara",
       artist: "The Prophec",
-      img: "5",
-      src: "5"
+      img: "img-5",
+      src: "music-5"
     },
     {
       name: "Excuses",
       artist: "AP Dhillon",
-      img: "6",
-      src: "6"
+      img: "img-6",
+      src: "music-6"
     },
     {
       name: "Moon Rise",
       artist: "Guru Randhawa",
-      img: "7",
-      src: "7"
+      img: "img-7",
+      src: "music-7"
     },
     {
       name: "Tu Ake Dekh Le",
       artist: "King",
-      img: "8",
-      src: "8"
+      img: "img-8",
+      src: "music-8"
     },
     {
       name: "Mi Amor",
       artist: "Sharn",
-      img: "9",
-      src: "9"
+      img: "img-9",
+      src: "music-9"
     },
     {
       name: "Akhiyaan",
       artist: "Mitraz",
-      img: "10",
-      src: "10"
+      img: "img-10",
+      src: "music-10"
     },
     {
       name: "Nasha X Old Town Road",
       artist: "Sush & Yohan",
-      img: "11",
-      src: "11"
+      img: "img-11",
+      src: "music-11"
     },
     {
       name: "Ranjha",
       artist: "B Praak",
-      img: "12",
-      src: "12"
+      img: "img-12",
+      src: "music-12"
     },
     {
       name: "Safar",
       artist: "Mix Singh",
-      img: "13",
-      src: "13"
+      img: "img-13",
+      src: "music-13"
     },
     {
       name: "Indian Riddim",
       artist: "Beats By Saheed",
-      img: "14",
-      src: "14"
+      img: "img-14",
+      src: "music-14"
     },
     {
       name: "Rangeen",
       artist: "Beats By Saheed",
-      img: "14",
-      src: "15"
+      img: "img-14",
+      src: "music-15"
     },
     {
       name: "Noor",
       artist: "Beats By Saheed",
-      img: "14",
-      src: "16"
+      img: "img-14",
+      src: "music-16"
     },
     {
       name: "Karishma",
       artist: "Beats By Saheed",
-      img: "14",
-      src: "17"
+      img: "img-14",
+      src: "music-17"
     },
     {
       name: "Pachtaoge",
       artist: "Arjit Singh",
-      img: "14",
-      src: "18"
+      img: "img-14",
+      src: "music-18"
     },
     {
       name: "Indian Flute",
       artist: "Beats By Saheed",
-      img: "14",
-      src: "19"
+      img: "img-14",
+      src: "music-19"
     },
     {
       name: "Faasle",
       artist: "Aditya Rikhari",
-      img: "14",
-      src: "20"
+      img: "img-14",
+      src: "music-20"
     },
   ];
