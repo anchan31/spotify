9393c2b6806a8f12f6150d5125dc914fcbeb1773
anchan31 <151496115+anchan31@users.noreply.github.com>
2025-03-30 12:36:01 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Import and configure Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCDMuiwxLNqH2sbDKIwQnjMc_E27rDPnEI",
+  authDomain: "anchan4746.firebaseapp.com",
+  projectId: "anchan4746",
+  storageBucket: "anchan4746.firebasestorage.app",
+  messagingSenderId: "358621818152",
+  appId: "1:358621818152:web:2e759be0619d1dd7acb41d",
+  measurementId: "G-DKYFD9ZNCL"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
