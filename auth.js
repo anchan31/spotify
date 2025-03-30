4e1c0a95ee8f2b9a94c51e17910619f2a9eb8c2e
anchan31 <151496115+anchan31@users.noreply.github.com>
2025-03-30 12:42:06 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,11 +1,12 @@
-// Firebase Configuration
+// Import and configure Firebase
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCDMuiwxLNqH2sbDKIwQnjMc_E27rDPnEI",
+  authDomain: "anchan4746.firebaseapp.com",
+  projectId: "anchan4746",
+  storageBucket: "anchan4746.firebasestorage.app",
+  messagingSenderId: "358621818152",
+  appId: "1:358621818152:web:2e759be0619d1dd7acb41d",
+  measurementId: "G-DKYFD9ZNCL"
 };
 
 // Initialize Firebase
