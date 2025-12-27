ddedb7e9ec3f6bcdd988584c862a14d35eb23c93
Chandan Singh <151496115+anchan31@users.noreply.github.com>
2025-12-27 21:52:44 +0530

    ver 2.0.2
    fix minor bugs and applied fixes

--- music-list.js
+++ music-list.js
@@ -3,356 +3,46 @@ let allMusic = [
     name: "Apna Bana Le",
     artist: "Arjit Singh",
     album: "Bhediya",
-    img: "img-1",
+    img: "apnabanale",
     artistImg: "arjit-singh",
     albumImg: "bhediya",
-    src: "music-1",
+    src: "apnabanale",
   },
-  {
-    name: "Players",
-    artist: "Badshah, Karan Aujla",
-    album: "Players EP",
-    img: "img-2",
-    artistImg: "badshah",
-    albumImg: "players-ep",
-    src: "music-2",
-  },
-  {
-    name: "Closer X Kesariya",
-    artist: "Sagar Swarap",
-    album: "Mashup Singles",
-    img: "img-3",
-    artistImg: "sagar-swarap",
-    albumImg: "mashup-singles",
-    src: "music-3",
-  },
-  {
-    name: "Passori",
-    artist: "Coke Studio Pak",
-    album: "Coke Studio Season 14",
-    img: "img-4",
-    src: "music-4"
-  },
-  {
-    name: "Dilawara",
-    artist: "The Prophec",
-    album: "Desi Beats",
-    img: "img-5",
-    src: "music-5"
-  },
-  {
-    name: "Excuses",
-    artist: "AP Dhillon",
-    album: "Excuses",
-    img: "img-6",
-    src: "music-6"
-  },
-  {
-    name: "Moon Rise",
-    artist: "Guru Randhawa",
-    album: "Moon Rise",
-    img: "img-7",
-    src: "music-7"
-  },
-  {
-    name: "Tu Ake Dekh Le",
-    artist: "King",
-    album: "The Carnival",
-    img: "img-8",
-    src: "music-8"
-  },
-  {
-    name: "Mi Amor",
-    artist: "Sharn",
-    album: "Mi Amor",
-    img: "img-9",
-    src: "music-9"
-  },
-  {
-    name: "Akhiyaan",
-    artist: "Mitraz",
-    album: "Mitraz",
-    img: "img-10",
-    src: "music-10"
-  },
-  {
-    name: "Nasha X Old Town Road",
-    artist: "Sush & Yohan",
-    album: "Mashup Singles",
-    img: "img-11",
-    src: "music-11"
-  },
-  {
-    name: "Ranjha",
-    artist: "B Praak",
-    album: "Shershaah",
-    img: "img-12",
-    src: "music-12"
-  },
-  {
-    name: "Safar",
-    artist: "Mix Singh",
-    album: "Safar",
-    img: "img-13",
-    src: "music-13"
-  },
-  {
-    name: "Indian Riddim",
-    artist: "Beats By Saheed",
-    album: "Beats Collection",
-    img: "music-placeholder",
-    src: "music-14"
-  },
-  {
-    name: "Rangeen",
-    artist: "Beats By Saheed",
-    album: "Beats Collection",
-    img: "music-placeholder",
-    src: "music-15"
-  },
-  {
-    name: "Noor",
-    artist: "Beats By Saheed",
-    album: "Beats Collection",
-    img: "music-placeholder",
-    src: "music-16"
-  },
-  {
-    name: "Karishma",
-    artist: "Beats By Saheed",
-    album: "Beats Collection",
-    img: "music-placeholder",
-    src: "music-17"
-  },
-  {
-    name: "Pachtaoge",
-    artist: "Arjit Singh",
-    img: "music-placeholder",
-    src: "music-18"
-  },
-  {
-    name: "Indian Flute",
-    artist: "Beats By Saheed",
-    img: "music-placeholder",
-    src: "music-19"
-  },
-  {
-    name: "Faasle",
-    artist: "Aditya Rikhari",
-    img: "img-20",
-    src: "music-20"
-  },
-  {
-    name: "Red Light",
-    artist: "Third Hour",
-    img: "img-21",
-    src: "music-21"
-  },
-  {
-    name: "Akhiyaan Gulaab",
-    artist: "Mitraz",
-    img: "img-22",
-    src: "music-22"
-  },
-  {
-    name: "Taroon Ke Neeche",
-    artist: "Third Hour",
-    img: "img-23",
-    src: "music-23"
-  },
-  {
-    name: "Lowkey",
-    artist: "Third Hour",
-    img: "img-24",
-    src: "music-24"
-  },
-  {
-    name: "Sawaal",
-    artist: "Third Hour",
-    img: "img-25",
-    src: "music-25"
-  },
-  {
-    name: "Miracle",
-    artist: "Third Hour",
-    img: "img-26",
-    src: "music-26"
-  },
-  {
-    name: "Deedaar",
-    artist: "Third Hour",
-    img: "music-placeholder",
-    src: "music-27"
-  },
-  {
-    name: "Pardesiyaan",
-    artist: "Third Hour",
-    img: "music-placeholder",
-    src: "music-28"
-  },
-  {
-    name: "Raajhna(Feat.JLok)",
-    artist: "Third Hour",
-    img: "music-placeholder",
-    src: "music-29"
-  },
-  {
-    name: "Fitoor",
-    artist: "Third Hour",
-    img: "music-placeholder",
-    src: "music-30"
-  },
-  {
-    name: "Galiyaan",
-    artist: "Third Hour",
-    img: "music-placeholder",
-    src: "music-31"
-  },
-  {
-    name: "Peher",
-    artist: "Third Hour",
-    img: "music-placeholder",
-    src: "music-32"
-  },
-  {
-    name: "L.A. Parties",
-    artist: "Third Hour",
-    img: "music-placeholder",
-    src: "music-33"
-  },
-  {
-    name: "Fallin",
-    artist: "Third Hour",
-    img: "music-placeholder",
-    src: "music-34"
-  },
-  {
-    name: "Mislead",
-    artist: "Third Hour",
-    img: "music-placeholder",
-    src: "music-35"
-  },
-  {
-    name: "Downtown",
-    artist: "Third Hour",
-    img: "music-placeholder",
-    src: "music-36"
-  },
-  {
-    name: "Gal Teri",
-    artist: "Third Hour",
-    img: "music-placeholder",
-    src: "music-37"
-  }
-  ,
-  {
-    name: "Music 38",
-    artist: "Unknown Artist",
-    img: "music-placeholder",
-    src: "music-38"
-  },
-  {
-    name: "Music 39",
-    artist: "Unknown Artist",
-    img: "music-placeholder",
-    src: "music-39"
-  },
-  {
-    name: "Music 40",
-    artist: "Unknown Artist",
-    img: "music-placeholder",
-    src: "music-40"
-  },
-  {
-    name: "Music 41",
-    artist: "Unknown Artist",
-    img: "img-41",
-    src: "music-41"
-  },
-  {
-    name: "Music 42",
-    artist: "Unknown Artist",
-    img: "img-42",
-    src: "music-42"
-  },
-  {
-    name: "Music 43",
-    artist: "Unknown Artist",
-    img: "img-43",
-    src: "music-43"
-  },
-  {
-    name: "Music 44",
-    artist: "Unknown Artist",
-    img: "img-44",
-    src: "music-44"
-  },
-  {
-    name: "Music 45",
-    artist: "Unknown Artist",
-    img: "img-45",
-    src: "music-45"
-  },
-  {
-    name: "Music 46",
-    artist: "Unknown Artist",
-    img: "img-46",
-    src: "music-46"
-  },
-  {
-    name: "Music 47",
-    artist: "Unknown Artist",
-    img: "img-47",
-    src: "music-47"
-  },
-  {
-    name: "Music 48",
-    artist: "Unknown Artist",
-    img: "img-48",
-    src: "music-48"
-  },
-  {
-    name: "Music 49",
-    artist: "Unknown Artist",
-    img: "music-placeholder",
-    src: "music-49"
-  },
-
   {
     name: "Nafrat",
     artist: "Darshan Raval",
     album: "I Loved You",
-    img: "img-50",
+    img: "nafrat",
     artistImg: "darshan-raval",
     albumImg: "ilovedyou",
-    src: "music-50",
+    src: "nafrat",
   },
   {
     name: "Deewana",
     artist: "Darshan Raval",
     album: "I Loved You",
-    img: "img-51",
+    img: "deewana",
     artistImg: "darshan-raval",
     albumImg: "ilovedyou",
-    src: "music-51",
+    src: "deewana",
   },
   {
     name: "Haara",
     artist: "Darshan Raval",
     album: "I Loved You",
-    img: "img-52",
+    img: "haara",
     artistImg: "darshan-raval",
     albumImg: "ilovedyou",
-    src: "music-52",
+    src: "haara",
   },
   {
     name: "Jiyaa",
     artist: "Darshan Raval",
     album: "I Loved You",
-    img: "img-53",
+    img: "jiyaa",
     artistImg: "darshan-raval",
     albumImg: "ilovedyou",
-    src: "music-53",
+    src: "jiyaa",
   },
 ];
 
